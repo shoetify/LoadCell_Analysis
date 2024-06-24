b7a92cf7cc9816d9c57de1ec1a6230bce51fd1df
shoetify <93328716+shoetify@users.noreply.github.com>
2024-06-24 14:43:07 +0800

    + wind speed calculation
    Signed-off-by: Difei Xiao <21656761@student.uwa.edu.au>

--- Util.py
+++ Util.py
@@ -1,3 +1,6 @@
+import re
+
+
 class LoadCell_Util:
     @staticmethod
     def parse_markdown(md_file_path):
@@ -15,6 +18,7 @@ class LoadCell_Util:
         # Variables to store data
         wind_speed = []
         start_time = []
+        end_time = []
         file_name = []
 
         # Flags and temporary storage
@@ -31,19 +35,21 @@ class LoadCell_Util:
                 if line.startswith("|"):
                     cells = [cell.strip() for cell in line.split('|')[1:-1]]
 
-                    if len(cells) == 3:
+                    if len(cells) == 4:
                         wind_speed.append(cells[0])
                         start_time.append(cells[1])
-                        file_name.append(cells[2])
+                        end_time.append(cells[2])
+                        file_name.append(cells[3])
 
                 else:
                     in_table = False  # End of the table
                     # Save the table data to the list of tables
-                    if wind_speed or start_time or file_name:
-                        tables.append([wind_speed.copy(), start_time.copy(), file_name.copy()])
+                    if wind_speed or start_time or end_time or file_name:
+                        tables.append([wind_speed.copy(), start_time.copy(), end_time.copy(), file_name.copy()])
                         # Clear the lists for the next table
                         wind_speed.clear()
                         start_time.clear()
+                        end_time.clear()
                         file_name.clear()
             elif line.startswith("| ---"):
                 in_table = True  # Table detected
@@ -69,3 +75,34 @@ class LoadCell_Util:
                         columns[idx].append(float(value))  # Convert to float and append to respective column list
 
         return columns
+
+    @staticmethod
+    def extract_numbers_from_string(string):
+
+        """
+        This function recognize all the number in the string and return a list of number.
+
+        :param string: A string with numbers (e.g., "y=0.1726x-0.06956").
+        :return: A list of float number representing the numbers in the string.
+        """
+
+        pattern = r'-?\d+\.?\d*'
+        numbers = re.findall(pattern, string)
+        return [float(num) for num in numbers]
+
+    @staticmethod
+    def motor_to_wind_speed(motorHz, wind_speed_a, wind_speed_b):
+
+        """
+        This function transfer the motor speed(Hz) into the real wind speed in wind tunnel.
+
+        :param: motorHz: float, the motor speed in Hz;
+                wind_speed_a: float, the wind speed coefficient get from config file;
+                wind_speed_b: float, the wind speed coefficient get from config file;
+        :return: float, the calculated real wind speed in wind tunnel.
+        """
+
+        if motorHz == 0.0:
+            return 0.0
+        else:
+            return motorHz * wind_speed_a + wind_speed_b
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from Util import LoadCell_Util
 
 # Read all the table in the log file
@@ -7,11 +8,29 @@ tables = []
 for file in os.listdir():
     if file.endswith('.md'):
         hasMD = True
+        print('Reading file [' + file + '] ...')
         tables = tables + LoadCell_Util.parse_markdown(file)
+
+
 
 # If no log file, then need to input the log manually.
 if not hasMD:
     raise TypeError("Cannot find any log in this folder!!!")
 
-columns = LoadCell_Util.read_txt_file("Full Scale.txt")
-print(len(columns[0]))
+# Read the config file
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# Get Wind speed relationship
+wind_speed_a, wind_speed_b = LoadCell_Util.extract_numbers_from_string(config['Parameter']['WindSpeed_relationship'])
+
+stable_time_0Hz = config['Parameter']['Stable_time_0Hz']
+stable_time_others = config['Parameter']['Stable_time_others']
+gap_before_next_wind_speed = config['Parameter']['Gap_before_next_wind_speed']
+
+
+# for table in tables:
+#     print(table)
+#
+# columns = LoadCell_Util.read_txt_file("Full Scale.txt")
+# print(len(columns[0]))
