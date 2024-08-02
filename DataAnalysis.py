4b4154e3b994334f1b5ce3e221a7f7b9eea42d2a
Difei Xiao <21656761@student.uwa.edu.au>
2024-08-02 15:49:14 +0800

    reconstruct the dataanalysis part
    Signed-off-by: Difei Xiao <21656761@student.uwa.edu.au>

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -3,6 +3,11 @@ import numpy as np
 
 
 class DataAnalyzer:
+    INDICATOR = ["Load-Cell 1's horizontal",
+                 "Load-Cell 1's vertical",
+                 "Load-Cell 2's horizontal",
+                 "Load-Cell 2's vertical"]
+
     @staticmethod
     def analyze(proceeded_tables, sample_rate, stable_time_others, stable_time_0Hz):
 
@@ -29,35 +34,30 @@ class DataAnalyzer:
                 # If it's a new file name, then read the file.
                 if proceeding_file_name != table[3][i]:
                     proceeding_file_name = table[3][i]
-                    column = LoadCell_Util.read_txt_file(proceeding_file_name)
-                    raw_data[0], raw_data[1], raw_data[2], raw_data[3] = column[0], column[1], column[6], column[7]
-                    column.clear()
+                    raw_data = DataAnalyzer.read_new_file(proceeding_file_name)
 
-                print(f'For wind speed {table[0][i]}')
+                print(f'Start analyse wind speed: {table[0][i]}')
 
                 start_time, end_time = table[1][i], table[2][i]
                 time_series = list(range(start_time * sample_rate, end_time * sample_rate + 1))
-
-                print(f"length of the raw data is:{len(raw_data[0])}")
-
                 for j in range(4):
-                    print(f"For the {j} column of data")
-                    print(f"    start time is: {start_time}, end time is:{end_time}, sample rate is:{sample_rate}")
+                    print(f"Analysing {DataAnalyzer.INDICATOR[j]} data")
+                    print(f"    Start time is:{start_time}s; End time is:{end_time}s")
 
+                    # Get the data in the time range, linear fit and fix the data according to the result of linear fit.
                     data = raw_data[j][start_time * sample_rate: end_time * sample_rate + 1]
                     slope, intercept = LoadCell_Util.linear_fit(time_series, data)
-                    print(f"    Slope: {slope}, Intercept: {intercept}")
+                    print(f"    Linear fit result: y = {slope}x + {intercept}")
+                    data = DataAnalyzer.deduct_error(data, accumulated_error[j], slope)
 
-                    for k in range(len(data)):
-                        data[k] = data[k] - accumulated_error[j] - slope * k
-
+                    # Calculate mean and RMS value of Data
                     mean_force.append(np.mean(data))
                     rms_force.append(np.sqrt(np.mean(np.square(data))))
-
                     print(
                         f"    mean value after correction is: {mean_force[-1]}; RMS value after correction is: "
                         f"{rms_force[-1]}")
 
+                    # Calculate accumulate error
                     test_time = stable_time_others
                     if table[0][i] < 0.1:
                         test_time = stable_time_0Hz
@@ -66,11 +66,27 @@ class DataAnalyzer:
 
                 mean_force_table.append([abs(mean_force[0] - mean_force[2]), mean_force[1] + mean_force[3]])
                 rms_force_table.append(rms_force)
-
                 print(' ')
 
             # Put the result back to force_tables
             mean_force_tables.append(mean_force_table)
             rms_force_tables.append(rms_force_table)
 
-        return (mean_force_tables, rms_force_tables)
+        return mean_force_tables, rms_force_tables
+
+    @staticmethod
+    def read_new_file(file_name):
+        data = []
+        column = LoadCell_Util.read_txt_file(file_name)
+        data.append(column[0])
+        data.append(column[1])
+        data.append(column[6])
+        data.append(column[7])
+        column.clear()
+        return data
+
+    @staticmethod
+    def deduct_error(data, accumulated_error, slope):
+        for k in range(len(data)):
+            data[k] = data[k] - accumulated_error - slope * k
+        return data
