1d50694fd45d6d7ed9978d9e957f7d2c54e3c7f2
Difei Xiao <21656761@student.uwa.edu.au>
2024-08-12 14:56:28 +0800

    Will keep the console log after running the program
    Signed-off-by: Difei Xiao <21656761@student.uwa.edu.au>

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 import os
-import time
-
 import yaml
 from Util import LoadCell_Util
 from DataAnalysis import DataAnalyzer
 
-# Read all the tables in the log file
-hasFiles = False
-tables = []
-for file in os.listdir():
-    if file.endswith('.md'):
-        hasFiles = True
-        print(f'Reading file [{file}] ...')
-        tables += LoadCell_Util.parse_markdown(file)
-        print(f'Successfully read file [{file}] ...')
-    elif file.endswith('log.xlsx') and not file.startswith('~'):
-        hasFiles = True
-        print(f'Reading file [{file}] ...')
-        tables += LoadCell_Util.parse_excel(file)
-        print(f'Successfully read file [{file}] ...')
+try:
+    # Read all the tables in the log file
+    hasFiles = False
+    tables = []
+    for file in os.listdir():
+        if file.endswith('.md'):
+            hasFiles = True
+            print(f'Reading file [{file}] ...')
+            tables += LoadCell_Util.parse_markdown(file)
+            print(f'Successfully read file [{file}] ...')
+        elif file.endswith('log.xlsx') and not file.startswith('~'):
+            hasFiles = True
+            print(f'Reading file [{file}] ...')
+            tables += LoadCell_Util.parse_excel(file)
+            print(f'Successfully read file [{file}] ...')
 
-print(tables)
+    # If no log file, then need to input the log manually.
+    if not hasFiles:
+        raise (TypeError("Cannot find any log in this folder!!!"))
 
-# If no log file, then need to input the log manually.
-if not hasFiles:
-    input("***************ERROR*****************\n"
-          "Cannot find any log in this folder!!!\n"
-          "*************************************")
-    raise (TypeError("Cannot find any log in this folder!!!"))
+    print('Lab log reading finish ...')
 
-print('Lab log reading finish ...')
+    print('Config file reading ... ')
+    # Read the config file
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    print('Config file reading successfully ...')
 
-print('Config file reading ... ')
-# Read the config file
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-print('Config file reading successfully ...')
+    # Get Wind speed relationship
+    wind_speed_a, wind_speed_b = LoadCell_Util.extract_numbers_from_string(config['Parameter']['WindSpeed_relationship'])
 
-# Get Wind speed relationship
-wind_speed_a, wind_speed_b = LoadCell_Util.extract_numbers_from_string(config['Parameter']['WindSpeed_relationship'])
+    # Change the raw log table into proceeded table and ready for calculation
+    stable_time_0Hz = config['Parameter']['Stable_time_0Hz']
+    stable_time_others = config['Parameter']['Stable_time_others']
+    gap_before_next_wind_speed = config['Parameter']['Gap_before_next_wind_speed']
+    proceeded_tables = LoadCell_Util.proceed_table(tables, stable_time_0Hz, stable_time_others, gap_before_next_wind_speed,
+                                                   wind_speed_a, wind_speed_b)
 
-# Change the raw log table into proceeded table and ready for calculation
-stable_time_0Hz = config['Parameter']['Stable_time_0Hz']
-stable_time_others = config['Parameter']['Stable_time_others']
-gap_before_next_wind_speed = config['Parameter']['Gap_before_next_wind_speed']
-proceeded_tables = LoadCell_Util.proceed_table(tables, stable_time_0Hz, stable_time_others, gap_before_next_wind_speed,
-                                               wind_speed_a, wind_speed_b)
+    print(" ")
 
-print(" ")
+    # Start analyzing the data
+    sample_rate = config['Parameter']['Sample_rate']
+    mean_tables, rms_tables = DataAnalyzer.analyze(proceeded_tables, sample_rate, stable_time_others, stable_time_0Hz)
 
-# Start analyzing the data
-sample_rate = config['Parameter']['Sample_rate']
-mean_tables, rms_tables = DataAnalyzer.analyze(proceeded_tables, sample_rate, stable_time_others, stable_time_0Hz)
+    LoadCell_Util.toExcel(proceeded_tables, mean_tables, rms_tables)
 
-LoadCell_Util.toExcel(proceeded_tables, mean_tables, rms_tables)
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    input("Press Enter to exit...")
